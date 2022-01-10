d3994ff72dd94509e96b918a3ad99040833cfba2
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-01-10 11:25:01 +0530

    Create ColorCodingVariableDefinitions.h
    Added a header file to contain the variable definitions

---
+++ ColorCodingVariableDefinitions.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <assert.h>
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+const char* MajorColorNames[5];
+const char* MinorColorNames[5];
+
+int numberOfMajorColors;
+int numberOfMinorColors;
+
+const int MAX_COLORPAIR_NAME_CHARS;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
