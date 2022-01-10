0108189906f082b344a668f74bf4342bfd2a9f7a
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-01-10 21:01:18 +0530

    Update ColorCodingUtility.c

--- ColorCodingUtility.c
+++ ColorCodingUtility.c
@@ -7,9 +7,10 @@ void printColorCodingPair(void)
     unsigned int majorColorVar = 0;
     unsigned int minorColorVar = 0;
     int colourCode = 1;
-  
+    printf("------- COLOR CODE REFERENCE MANUAL -------\n\n"); 
     for(majorColorVar = 0; majorColorVar < MAJOR_AND_MINOR_COLORS ; majorColorVar++)
     {
+        printf("\n------- ********************************************************* -------");
         for(minorColorVar = 0; minorColorVar < MAJOR_AND_MINOR_COLORS ; minorColorVar++)
         {
              printf("\n Major Colour Name : %s  ", majorColorNames[majorColorVar]);
