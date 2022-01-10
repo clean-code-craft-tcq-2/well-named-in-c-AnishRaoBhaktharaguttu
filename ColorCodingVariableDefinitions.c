66e512758bebdf4ebab2247e2c599453589c8c79
Anish Rao Bhaktharaguttu <50461625+AnishRaoBhaktharaguttu@users.noreply.github.com>
2022-01-10 17:33:22 +0530

    Update ColorCodingVariableDefinitions.c
    updating names according to naming conventions

--- ColorCodingVariableDefinitions.c
+++ ColorCodingVariableDefinitions.c
@@ -10,7 +10,7 @@ const char* MinorColorNames[] = {"Blue", "Orange", "Green", "Brown", "Slate"};
 int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
-ColorPair GetColorFromPairNumber(int pairNumber) {
+ColorPair getColorFromPairNumber(int pairNumber) {
     ColorPair colorPair;
     int zeroBasedPairNumber = pairNumber - 1;
     colorPair.majorColor = 
@@ -20,13 +20,13 @@ ColorPair GetColorFromPairNumber(int pairNumber) {
     return colorPair;
 }
 
-void ColorPairToString(const ColorPair* colorPair, char* buffer) {
+void colorPairToString(const ColorPair* colorPair, char* buffer) {
     sprintf(buffer, "%s %s",
         MajorColorNames[colorPair->majorColor],
         MinorColorNames[colorPair->minorColor]);
 }
 
-int GetPairNumberFromColor(const ColorPair* colorPair) {
+int getPairNumberFromColor(const ColorPair* colorPair) {
     return colorPair->majorColor * numberOfMinorColors +
             colorPair->minorColor + 1;
 }
